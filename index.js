fcb25fc221905c48acebd218996fd3712171bf8a
Nicole Cheng <hy.nicolecheng@berkeley.edu>
2024-11-04 16:51:07 -0800

    get geofips api + activity db

--- index.js
+++ index.js
@@ -5,57 +5,143 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-const results = [];
+const stuff = [];
+// fs.createReadStream("inventory_data_bau.csv")
+//   .pipe(csv())
+//   .on('data', (data) => stuff.push(data))
+//   .on('end', () => {
+//     const filtered = stuff.filter(results => results.location_type.includes('City'));
+//    // const s = filtered.filter(filtered => filtered.geofips.includes(rq));
+//     console.log(filtered);
 
-fs.createReadStream('inventory_data_bau.csv')
+//     // const filteredItems = s.map(item => {
+//     //   let filteredItem = {};
+//     //   columns.forEach(field => {
+//     //     // Only add the field if it exists in the original item
+//     //     if (item[field]) {
+//     //       filteredItem[field] = item[field];
+//     //     }
+//     //   });
+//     //   return filteredItem;
+//     // });
+//     // filteredUsers = filteredUsers + filteredItems;
+// });
+console.log("hiihi");
+
+// app.get("/api/:geofips", (req, res) => {
+//     res.send(req.params.geofips);
+// });
+
+
+app.get("/test/:geofips", (request, response) => {
+  //Basic API call for geofips filter for one csv file
+  const rq2 = request.params.geofips;
+  const results = [];
+  fs.createReadStream("inventory_data_bau.csv")
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    const filtered = results.filter(results => results.location_type.includes("City"));
+    console.log(rq2);
+    const filtered = results.filter(results => results.geofips == rq2);
+    console.log(filtered);
 
+    return response.status(200).json(filtered);
   });
-app.get("/", (req, res) => {
-    res.send("hello");
-})
-app.get("/api/location_type/:location_type/geofips/:geofips/?array", (request, response) => {
-    //get all the users that have filter (username) as some value ('a')
-   // const location_type = request; //no table, no years (years is columns)
-    console.log("what");
-    const rq = request.params.location_type;
+
+});
+app.get("/api/:geofips", async (request, response) => {
+    //get all the info from all spreadsheets on this GEOFIPS filter
     const rq2 = request.params.geofips;
-    const rq3 = request.params.columns;
-    var arr = JSON.parse(request.query.array);
-    return response.status(200).send(filters(rq, rq2, arr));
+    const rq4 = request.query.spreadsheets;
+
+    //error checking for the array parameters + configure into array for usage
+    if (!rq4) {
+      return response.status(400).json({ error: 'No parameters provided' });
+    }
+
+    //let columns = rq3.flatMap(param => param.split(','));
+    var spreadsheets = rq4.split(',');
+
+    const filteredGeofips = await filters(rq2, spreadsheets);
+    //console.log(filteredGeofips);
+    return response.status(200).json(filteredGeofips);
 });
 
-function filters(location_type, geofips, columns) {
-    //filter by location: city
-    const filtered = results.filter(results => results.location_type.includes(location_type));
-    const s = filtered.filter(filtered => filtered.geofips.includes(geofips));
-    console.log(s);
-    console.log("here");
 
-    const filteredUsers = s.map(user => {
-        let filteredUser = {};
-        
-        // Loop through the selected parameters and add them to the new object
-        columns.forEach(param => {
-          if (user.hasOwnProperty(param)) {
-            filteredUser[param] = user[param];
-          }
-        });
-    
-        return filteredUser;
-      });
-    
-    //filter by geofips
-  //  filtered = filtered.result.rows.filter(filtered.geofips.includes(geofips))
-    return filteredUsers;
-    //filter by years
-   // filtered = filtered.result.rows.filter(filtered.year.includes(years))
-    
+async function filters(geofips, spreadsheets) {
+  //console.log(spreadsheets);
+   const filteredUsers = [];
+  // const filtered = [];
+   const s = [];
+   for (let i = 0; i < spreadsheets.length; i++) {
+     const results = await readCSV(spreadsheets[i]);
+     const filtered = results.filter(result => result.geofips.includes(geofips));
+     filteredUsers.push(...filtered);
+   }
+   return filteredUsers;
+     
+ };
+
+app.get("/activity/:geofips", async (request, response) => {
+  //get activity database
+  const rq2 = request.params.geofips;
+  const rq4 = request.query.spreadsheets;
+
+  //error checking for the array parameters + configure into array for usage
+  if (!rq4) {
+    return response.status(400).json({ error: 'No parameters provided' });
+  }
+
+  //let columns = rq3.flatMap(param => param.split(','));
+  var spreadsheets = rq4.split(',');
+
+  const filteredGeofips = await activityFilters(rq2, spreadsheets);
+  //console.log(filteredGeofips);
+  return response.status(200).json(filteredGeofips);
+});
+
+
+async function activityFilters(geofips, spreadsheets) {
+  const results1 = [];
+  const results2 = [];
+  const results3 = [];
+  const results4 = [];
+   const filteredUsers = [];
+   const s = [];
+   for (let i = 0; i < spreadsheets.length; i++) { //spreadsheet order, 
+     const csv = await readCSV(spreadsheets[i]);
+     const filtered = csv.filter(result => result.geofips.includes(geofips));
+     filteredUsers.push(...filtered);
+     if (spreadsheets[i] == "db.replica") {
+      console.log('we made it replica');
+      const temp = filtered.map((element)=>({2023:element["2023"]}));
+      console.log(temp);
+      results1.push(...temp);
+     } else if (spreadsheets[i] == "aadt2013_2019") {
+      console.log('we made it aadt');
+      for (let j = 13; j < 23; j++) {
+        const num = 2000 + j;
+        const temp = filtered.map((element)=>({[num]:element[num]}));
+        results1.push(...temp);
+      }
+     }
+   }
+  return results1;
+     
+ };
+
+
+function readCSV(file) {
+  return new Promise((resolve, reject) => {
+      const results = [];
+      fs.createReadStream(file + ".csv")
+          .pipe(csv())
+          .on('data', (data) => results.push(data))
+          .on('end', () => resolve(results))
+          .on('error', (error) => reject(error));
+  });
 }
 
 app.listen(PORT, () => {
-    console.log('Running on port ${PORT}');
+    console.log('Running on port ' + PORT);
 });
